17869e888b79cc04cc9bd713ed80fc83e0a801bf
Andersson Velasquez <cxasper23@gmail.com>
2018-02-02 16:52:49 -0500

    Create SpaceUser model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202210909) do
+ActiveRecord::Schema.define(version: 20180202214501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,17 @@ ActiveRecord::Schema.define(version: 20180202210909) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_spaces", force: :cascade do |t|
+    t.date "start_date"
+    t.date "finish_date"
+    t.bigint "space_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["space_id"], name: "index_user_spaces_on_space_id"
+    t.index ["user_id"], name: "index_user_spaces_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -110,5 +121,7 @@ ActiveRecord::Schema.define(version: 20180202210909) do
   add_foreign_key "maintenances", "assets"
   add_foreign_key "maintenances", "suppliers"
   add_foreign_key "spaces", "spaces"
+  add_foreign_key "user_spaces", "spaces"
+  add_foreign_key "user_spaces", "users"
   add_foreign_key "users", "organizations"
 end
---
+++ spec/factories/user_spaces.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user_space do
+    start_date '2018-02-02'
+    finish_date '2018-02-02'
+    space nil
+    user nil
+  end
+end
