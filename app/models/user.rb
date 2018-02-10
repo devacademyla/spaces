bbcc5cd6b0d29fc04f8e893df3303d1232a3d6e5
Victor Soto <victorm22560@gmail.com>
2018-02-09 22:31:52 -0500

    fixing codeclimate issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, 
-         :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
