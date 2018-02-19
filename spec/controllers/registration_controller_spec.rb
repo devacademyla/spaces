92780619d5a315c80480b721da89bff601c748bd
Moisés Ñañez <moisesnandres@gmail.com>
2018-02-18 19:53:41 -0500

    Fix spec

--- spec/controllers/registration_controller_spec.rb
+++ spec/controllers/registration_controller_spec.rb
@@ -19,7 +19,7 @@ describe Users::RegistrationsController, type: :controller do
         organization = create(:organization)
         user_params = attributes_for(:user, organization_id: organization.id)
         user_params = user_params.merge(password_confirmation: user_params[:password])
-        post :create, params: user_params
+        post :create, params: { user: user_params }
       end.to change(User, :count).by(1)
     end
   end
